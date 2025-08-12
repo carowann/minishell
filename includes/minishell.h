6de468e91838d319e34c61bf00b160edbc001790
carowann <wannheden.caroline@gmail.com>
2025-08-12 18:28:48 +0200

    feat: defined token_type, token, token_list, state

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: cwannhed <cwannhed@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 12:32:17 by cwannhed          #+#    #+#             */
-/*   Updated: 2025/08/11 13:18:26 by cwannhed         ###   ########.fr       */
+/*   Updated: 2025/08/12 18:08:51 by cwannhed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,39 @@
 # include <unistd.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+typedef enum e_token_type
+{
+	WORD,
+	PIPE,
+	REDIRECT_IN,
+	REDIRECT_OUT,
+	APPEND,
+	HEREDOC,
+	QUOTED_STRING,
+	VARIABLE
+} t_token_type;
+
+typedef struct s_token
+{
+	t_token_type	type;
+	char			*content;
+	struct s_token	*next;
+} t_token;
+
+typedef struct s_token_list
+{
+	t_token			*head;
+	int				count;
+} t_token_list;
+
+typedef enum e_state
+{
+	DEFAULT,
+	IN_SINGLE_QUOTE,
+	IN_DOUBLE_QUOTE,
+	IN_VARIABLE,
+	IN_OPERATOR
+}	t_state;
 
 #endif
