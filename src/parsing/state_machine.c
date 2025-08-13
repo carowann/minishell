083edb9eb4b2b760d493f6564b009821b0c3eaf0
carowann <wannheden.caroline@gmail.com>
2025-08-13 13:02:35 +0200

    feat: completed handle_default_state()

--- src/parsing/state_machine.c
+++ src/parsing/state_machine.c
@@ -6,7 +6,7 @@
 /*   By: cwannhed <cwannhed@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 19:07:47 by cwannhed          #+#    #+#             */
-/*   Updated: 2025/08/12 20:05:33 by cwannhed         ###   ########.fr       */
+/*   Updated: 2025/08/13 13:00:42 by cwannhed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,52 @@ void handle_state_machine(char c, t_parser *parser)
 	if (parser->state == DEFAULT)
 		handle_default_state(c, parser);
 	else if (parser->state == IN_DOUBLE_QUOTES)
-		handle_double_quotes(c, parser);
+		handle_double_quotes(c, parser); //TODO
 	else if (parser->state == IN_SINGLE_QUOTES)
-		handle_single_quotes(c, parser);
+		handle_single_quotes(c, parser); //TODO
 	else if (parser->state == IN_VARIABLE)
-		handle_variable_state(c, parser);
+		handle_variable_state(c, parser); //TODO
 	else if (parser->state == IN_OPERATOR)
-		handle_operator_state(c, parser);
+		handle_operator_state(c, parser); //TODO
 }
 
 void handle_default_state(char c, t_parser *parser)
 {
-	if (c = '"')
-	{
+	if (c == '"')
 		parser->state = IN_DOUBLE_QUOTES;
-		//buffer
-	}
 	else if (c == '\'')
-	{	
-		//
-	}
+		parser->state = IN_SINGLE_QUOTES;
 	else if (c == '$')
-	{
 		parser->state = IN_VARIABLE;
-		//buffer
+	else if (ft_isspace(c))
+	{
+		if (parser->buffer_pos > 0)
+		{
+			create_and_add_token(parser); //TODO
+			reset_buffer(parser); //TODO
+		}
 	}
-	//ecc
+	else if (c == '<' || c =='>')
+	{
+		if (parser->buffer_pos > 0)
+		{
+			create_and_add_token(parser);
+			reset_buffer(parser);
+		}
+		parser->state = IN_OPERATOR;
+	}
+	else if (c == '|')
+	{
+		if (parser->buffer_pos > 0)
+		{
+			create_and_add_token(parser);
+			reset_buffer(parser);
+		}
+		create_pipe_token(parser); //TODO
+	}
+	else
+	{
+		parser->buffer[parser->buffer_pos] = c;
+		parser->buffer_pos++;
+	}
 }
