d6df5ec5ddf4d9411ca8fb92367d0097ed5264a2
carowann <wannheden.caroline@gmail.com>
2025-08-13 15:25:56 +0200

    feat: free token and token list

---
+++ src/parsing/cleanup.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleanup.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwannhed <cwannhed@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/13 14:45:58 by cwannhed          #+#    #+#             */
+/*   Updated: 2025/08/13 15:23:08 by cwannhed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	free_token(t_token *token)
+{
+	if (!token)
+		return ;
+	if (token->content)
+		free(token->content);
+	free(token);
+	token = NULL;
+}
+
+void	free_token_list(t_token_list *token_list)
+{
+	t_token	*curr_token;
+	t_token	*temp_token;
+	
+	if (!token_list)
+		return ;
+	curr_token = token_list->head;
+	while (curr_token && curr_token->next)
+	{
+		temp_token = curr_token->next;
+		free_token(curr_token);
+		curr_token = temp_token;
+	}
+	free_token(curr_token);
+	free(token_list);
+}
