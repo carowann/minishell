2bb31a8090878bfc657bb795d179138a06b84aca
carowann <wannheden.caroline@gmail.com>
2025-08-13 15:53:26 +0200

    fix: tokenize() new return value for errors

--- src/parsing/tokenizer.c
+++ src/parsing/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: cwannhed <cwannhed@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 16:31:58 by cwannhed          #+#    #+#             */
-/*   Updated: 2025/08/13 14:39:04 by cwannhed         ###   ########.fr       */
+/*   Updated: 2025/08/13 15:51:15 by cwannhed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,29 +25,37 @@ static t_parser init_parser(t_parser parser, char *buffer, t_token_list *tokens)
 	return (parser);
 }
 
-t_token_list *tokenize(char *input)
+int	tokenize(char *input,  t_token_list **tokens)
 {
-	t_token_list	*tokens;
+	t_token_list	*temp_tokens;
 	t_state			state;
 	char			*buffer;
 	t_parser		parser;
 	
-	tokens = ft_calloc(1, sizeof(t_token_list));
-	if (!tokens)
-		handle_error(); //TODO
+	temp_tokens = ft_calloc(1, sizeof(t_token_list));
+	if (!temp_tokens)
+		return (-1);
 	buffer = ft_calloc(1, sizeof(ft_strlen(input) + 1));
 	if (!buffer)
-		handle_error();
-	*tokens = (t_token_list){0};
-	parser = init_parser(parser, buffer, tokens);
+	{
+		free_token_list(temp_tokens);
+		return (-1);		
+	}
+	*temp_tokens = (t_token_list){0};
+	parser = init_parser(parser, buffer, temp_tokens);
 	state = DEFAULT;
 	while (*input)
 	{
-		handle_state_machine(*input, &parser);
+		if (handle_state_machine(*input, &parser) == -1)
+		{
+			free_token_list(temp_tokens);
+			return (-1);
+		}
 		input++;
 	}
+	*tokens = temp_tokens;
 	free(buffer);
-	return (tokens);
+	return (0);
 }
 
 t_token	*create_token(t_parser *parser, t_token_type type)
@@ -56,7 +64,7 @@ t_token	*create_token(t_parser *parser, t_token_type type)
 
 	token = ft_calloc(1, sizeof(t_token));
 	if (!token)
-		handle_error(); //TODO
+		handle_error(); //TODO handle_error
 	token->type = type;
 	if (type == PIPE)
 		token->content = ft_strdup("|");
