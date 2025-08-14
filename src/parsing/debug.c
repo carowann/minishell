1a29de7580e0519aaf2659f55445c6975a0434c7
carowann <wannheden.caroline@gmail.com>
2025-08-14 19:20:08 +0200

    debug print token list

---
+++ src/parsing/debug.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwannhed <cwannhed@student.42firenze.it>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/14 19:02:52 by cwannhed          #+#    #+#             */
+/*   Updated: 2025/08/14 19:12:59 by cwannhed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void print_token_list(t_token_list *tokens)
+{
+	t_token	*current;
+	int		i;
+
+	i = 0;
+	if (!tokens || !tokens->head)
+	{
+		printf("Token list is empty\n");
+		return ;
+	}
+	printf("=== TOKEN LIST (%d tokens) ===\n", tokens->count);
+	current = tokens->head;
+	while (current)
+	{
+		printf("[%d] Type: %d, Content: \"%s\"\n",
+				i , current->type, current->content);
+		current = current->next;
+		i++;
+	}
+	printf("=== END TOKEN LIST ===\n");
+}
