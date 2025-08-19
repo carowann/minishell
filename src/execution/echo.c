283a9c227056fdbfc332cca0a57607382eb6c30d
Ludovico Zorzit <lzorzit@c2r2p3.42firenze.it>
2025-08-19 17:36:22 +0200

    echo bare bone finished, no leaks, to ad more checks

--- src/execution/echo.c
+++ src/execution/echo.c
@@ -6,21 +6,24 @@
 /*   By: lzorzit <lzorzit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 18:02:22 by lzorzit           #+#    #+#             */
-/*   Updated: 2025/08/18 18:05:16 by lzorzit          ###   ########.fr       */
+/*   Updated: 2025/08/19 17:34:54 by lzorzit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../../libft/libft.h"
 
-//	convert matrix to string, TODO move to another file for use in other function and check memoory leaks
+//	convert matrix to string, TODO move to another file for use in other function
 char *conv_to_strn(char	**args)
 {
 	char	*str; //return
 	int		i; //index
 
-	i = 0;
-	if (ft_strncmp(args[0], "-n", 3) == 0)
+	i = 1;
+	if (ft_strncmp(args[1], "-n", 3) == 0)
 		i++;
+	if(args[i])
+		str = ft_strndup(args[i], ft_strlen(args[i]));
+	i++;
 	while (args[i])
 	{
 		str = ft_strjoin(str, args[i]);
@@ -30,13 +33,32 @@ char *conv_to_strn(char	**args)
 	}
 	return(str);
 }
+//	execution of command after vars are passed by echo
+int	echo_exec(char *str, int n_var, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(fd, &str[i], 1);
+		i++;
+	}
+	if(n_var == 0)
+		write(fd, "\n", 1);
+	if(str)
+		free(str);
+	return(1);
+}
 //	command called when the Echo keyword is read
 int	echo(char	**args, int redirect_fd)
 {
 	int		n_var;
 
+	if (!args[1])
+		return(-1);
 //	check for -n option, witch skips the terminating /n
-	if (args[0] == "-n")
+	if (ft_strncmp(args[1], "-n", 3) == 0)
 		n_var = 1;
 	else 
 		n_var = 0;
@@ -45,27 +67,12 @@ int	echo(char	**args, int redirect_fd)
 	return(1);
 }
 
-//	execution of command after vars are passed by echo
-int	echo_exec(char *str, int n_var, int fd)
-{
-	int	i;
 
-	i = 0;
-	while (str[i]!= '\0')
-	{
-		write(fd, str[i], 1);
-		i++;
-	}
-	if(n_var == 0)
-		write(fd, '\n', 1);
-	if(str)
-		free(str);
-	return(1);
-}
 
 // int	main(int argc, char **argv)
 // {
-// 	echo(argv, 1);
+// 	if (argc)
+// 		echo(argv, 1);
 // 	return(0);
 // }
 
