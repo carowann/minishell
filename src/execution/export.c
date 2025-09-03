1fd86a174f8b079fb1eac807cdb89ab3aac859df
carowann <cwannhed@student.42firenze.it>
2025-09-03 14:57:47 +0200

    fixed export: better check for valid identifier and fixed returrn value for exit status

--- src/execution/export.c
+++ src/execution/export.c
@@ -6,35 +6,58 @@
 /*   By: cwannhed <cwannhed@student.42firenze.it>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 13:49:20 by lzorzit           #+#    #+#             */
-/*   Updated: 2025/09/02 22:25:52 by cwannhed         ###   ########.fr       */
+/*   Updated: 2025/09/03 14:56:01 by cwannhed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-//TODO: sort env in alphabetical order
+// TODO: sort env in alphabetical order
+static int is_valid_identifier(char *str);
 
 int export(t_cmd *cmd, t_shell_state **shell, int fd)
 {
 	int i;
+	int has_error = 0;
 
 	i = 1;
 	if (!cmd->args[1])
 		return (env((*shell)->env_list, fd, 1));
 	while (cmd->args[i])
 	{
-		if (ft_isalpha(cmd->args[i][0]) == 0 && cmd->args[i][0] != '_')
+		if (!is_valid_identifier(cmd->args[i]))
 		{
 			ft_printf("minishell: export: `%s': not a valid identifier\n", cmd->args[i]);
-			return (1);
+			has_error = 1;
 		}
-		if (!find_env((*shell)->env_list, cmd->args[i]))
-			add_env(&((*shell)->env_list), cmd->args[i]);
 		else
-			update_env((*shell)->env_list, cmd->args[i]);
+		{
+			if (!find_env((*shell)->env_list, cmd->args[i]))
+				add_env(&((*shell)->env_list), cmd->args[i]);
+			else
+				update_env((*shell)->env_list, cmd->args[i]);
+		}
 		i++;
 	}
-	return (SUCCESS);
+	return (has_error);
+}
+
+static int is_valid_identifier(char *str)
+{
+	int i;
+
+	if (!str || !str[0])
+		return (0);
+	if (!ft_isalpha(str[0]) && str[0] != '_')
+		return (0);
+	i = 1;
+	while (str[i] && str[i] != '=')
+	{
+		if (!ft_isalnum(str[i]) && str[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 // Function to add a new environment variable to the list
@@ -53,13 +76,11 @@ int add_env(t_env **envar, char *arg)
 		return (-1);
 	}
 	new->next = NULL;
-
 	if (!*envar)
 	{
 		*envar = new;
 		return (1);
 	}
-
 	temp = *envar;
 	while (temp->next)
 		temp = temp->next;
